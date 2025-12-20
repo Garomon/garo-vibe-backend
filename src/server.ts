6fbaeb6fe3eccb0c37053cc18f0b47a0da257ee9
Garo <garo.montoya@gmail.com>
2025-12-19 19:05:16 -0600

    Fix Express 5 route error for Blinks middleware

--- src/server.ts
+++ src/server.ts
@@ -126,7 +126,7 @@ app.post('/mint', async (req, res) => {
 // --- SOLANA ACTIONS (BLINKS) CONFIG ---
 
 // Middleware to set specific CORS for Actions (Must allow all origins)
-app.use('/api/actions/*', (req, res, next) => {
+app.use('/api/actions', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Encoding, Accept-Encoding');
