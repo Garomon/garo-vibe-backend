c95eb31c9a7f0f423996f19b41d2bd59a368be0c
Garo <garo.montoya@gmail.com>
2025-12-28 14:31:03 -0600

    UX: Hide language toggle on admin routes

--- web/src/app/components/GlobalLanguageToggle.tsx
+++ web/src/app/components/GlobalLanguageToggle.tsx
@@ -1,8 +1,16 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { LanguageToggle } from "../../context/LanguageProvider";
 
 export function GlobalLanguageToggle() {
+    const pathname = usePathname();
+
+    // Hide on admin routes
+    if (pathname?.startsWith("/admin")) {
+        return null;
+    }
+
     return (
         <div className="fixed bottom-4 right-4 z-[999]">
             <LanguageToggle />
